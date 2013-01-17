befa560e1ec5da1a99d601169f47c4951c239ed1
radek <radek@silicon.(none)>
2013-01-17 12:09:36 +0100

    api version

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,7 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "ErisSG",
-    version := "1.0.0",
+    version := "1.0.1",
     versionCode := 0,
     scalaVersion := "2.9.2",
     platformName in Android := "android-8"
@@ -23,7 +23,7 @@ object General {
     proguardSettings ++
     AndroidManifestGenerator.settings ++
     Seq (
-      libraryDependencies += "rtkaczyk.eris" % "api" % "0.1"
+      libraryDependencies += "rtkaczyk.eris" % "api" % "1.0.0"
     )
 }
 
